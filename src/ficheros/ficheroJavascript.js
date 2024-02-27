5a6c9fa7ecd8f4d9e9c8ffdeb862c67c3ff71d42
igarrastazu <202114450@alu.comillas.edu>
2024-02-27 13:38:10 +0100

    el quiosco v4

--- src/ficheros/ficheroJavascript.js
+++ src/ficheros/ficheroJavascript.js
@@ -1,6 +1,27 @@
 const newsProxy = {"status":"ok","totalResults":10,"articles":[{"source":{"id":"techcrunch","name":"TechCrunch"},"author":"Amanda Silberling","title":"Reddit cites r/WallStreetBets as a risk factor in its IPO filing | TechCrunch","description":"As Reddit finally files to go public, the company wrote in its S-1 filing that \"meme stock\" schemes on r/WallStreetBets could pose a risk to investors.","url":"https://techcrunch.com/2024/02/23/reddit-wallstreetbets-risk-factor-ipo/","urlToImage":"https://techcrunch.com/wp-content/uploads/2023/11/snoo_expressions.png?resize=1200,675","publishedAt":"2024-02-23T17:42:07Z","content":"As Reddit finally files to go public, the company wrote in its S-1 filing that “meme stock” schemes on r/WallStreetBets could pose a risk to investors.\r\nThe subreddit r/WallStreetBets, a community of… [+1991 chars]"},{"source":{"id":"techcrunch","name":"TechCrunch"},"author":"Dominic-Madori Davis","title":"Mutale Nkonde's nonprofit is working to make AI less biased | TechCrunch","description":"TechCrunch has launched a series of interviews focusing on remarkable women who’ve contributed to the AI revolution.","url":"https://techcrunch.com/2024/02/23/mutale-nkondes-nonprofit-is-working-to-make-ai-less-biased/","urlToImage":"https://techcrunch.com/wp-content/uploads/2024/02/women-in-ai-nkonde.jpg?resize=1200,675","publishedAt":"2024-02-23T17:05:39Z","content":"To give AI-focused women academics and others their well-deserved and overdue time in the spotlight, TechCrunch is launching a series of interviews focusing on remarkable women whove contributed to t… [+6146 chars]"},{"source":{"id":"techcrunch","name":"TechCrunch"},"author":"Lorenzo Franceschi-Bicchierai","title":"Spyware leak offers 'first-of-its-kind' look inside Chinese government hacking efforts | TechCrunch","description":"Leaked documents show Chinese contractor I-Soon developed hacking tools targeting iPhones, Macs, Android devices and Windows PCs.","url":"https://techcrunch.com/2024/02/23/isoon-china-government-hacking-spyware-leak/","urlToImage":"https://techcrunch.com/wp-content/uploads/2024/02/isoon-green-china-ap-spyware-1.jpg?resize=1200,800","publishedAt":"2024-02-23T16:07:10Z","content":"Over the weekend, someone posted a cache of files and documents apparently stolen from the Chinese government hacking contractor, I-Soon.\r\nThis leak gives cybersecurity researchers and rival governme… [+6003 chars]"},{"source":{"id":"techcrunch","name":"TechCrunch"},"author":"Aisha Malik","title":"Google is sunsetting the Google Pay app in the US later this year | TechCrunch","description":"Google has announced that Google Pay is shutting down in the United States in June, as the standalone app has been replaced by Google Wallet","url":"https://techcrunch.com/2024/02/23/google-is-sunsetting-the-google-pay-app-in-the-us-later-this-year/","urlToImage":"https://techcrunch.com/wp-content/uploads/2021/07/GettyImages-1207206237.jpg?resize=1200,800","publishedAt":"2024-02-23T16:02:39Z","content":"Google has announced that Google Pay is shutting down in the United States in June, as the standalone app has largely been replaced by Google Wallet. The company says the move is designed to simplify… [+1460 chars]"},{"source":{"id":"techcrunch","name":"TechCrunch"},"author":"Alex Wilhelm, Mary Ann Azevedo, Kirsten Korosec and Theresa Loconsolo","title":"Match Group's deal with OpenAI is just business with AI glitter on top | TechCrunch","description":"The Equity crew is covering deals of the week from PermitFlow, Cake and Match Group, Klarna's board, and which startups could be 2024's winners.","url":"https://techcrunch.com/2024/02/23/match-groups-deal-with-openai-is-just-business-with-ai-glitter-on-top/","urlToImage":"https://techcrunch.com/wp-content/uploads/2023/12/openai-sam-altman.jpg?w=1200","publishedAt":"2024-02-23T15:03:26Z","content":"Listen here or wherever you get your podcasts.\r\nHello, and welcome to Equity, a podcast about the business of startups, where we unpack the numbers and nuance behind the headlines.\r\nThis week, Mary A… [+644 chars]"},{"source":{"id":"techcrunch","name":"TechCrunch"},"author":"Mike Butcher","title":"Two years since Russia's invasion, Ukraine's startups soldier on | TechCrunch","description":"This weekend marks exactly two years since Russia’s illegal, unprovoked and brutal invasion of Ukraine. Despite overwhelming odds and continued hiccups in","url":"https://techcrunch.com/2024/02/23/two-years-since-russias-invasion-ukraines-startups-soldier-on/","urlToImage":"https://techcrunch.com/wp-content/uploads/2022/06/GettyImages-1388071988.jpg?resize=1200,798","publishedAt":"2024-02-23T13:28:05Z","content":"This weekend marks exactly two years since Russias illegal, unprovoked and brutal invasion of Ukraine. Despite overwhelming odds and continued hiccups in the supplies of Western aid to fight off Russ… [+6874 chars]"},{"source":{"id":"techcrunch","name":"TechCrunch"},"author":"Manish Singh","title":"Byju's investors unanimously vote to remove founder | TechCrunch","description":"A group of Byju's investors on Friday voted to remove the edtech group's founder and chief executive Byju Raveendran and separately filed an oppression","url":"https://techcrunch.com/2024/02/23/byjus-investors-unanimously-vote-to-remove-founder/","urlToImage":"https://techcrunch.com/wp-content/uploads/2023/11/GettyImages-1257740205.jpg?resize=1200,800","publishedAt":"2024-02-23T13:05:27Z","content":"A group of Byju’s investors on Friday voted to remove the edtech group’s founder and chief executive Byju Raveendran and separately filed an oppression and management suit against the leadership of t… [+2539 chars]"},{"source":{"id":"techcrunch","name":"TechCrunch"},"author":"Carly Page","title":"Hackers are exploiting ConnectWise flaws to deploy LockBit ransomware, security experts warn | TechCrunch","description":"Security experts are warning that a pair of high-risk ConnectWise flaws are being exploited by hackers to deploy LockBit ransomware","url":"https://techcrunch.com/2024/02/23/hackers-are-exploiting-connectwise-flaws-to-deploy-lockbit-ransomware-security-experts-warn/","urlToImage":"https://techcrunch.com/wp-content/uploads/2023/12/ransomware-bugs-black-Samuil_Levich-getty.jpg?resize=1200,800","publishedAt":"2024-02-23T08:55:10Z","content":"Security experts are warning that a pair of high-risk flaws in a popular remote access tool are being exploited by hackers to deploy LockBit ransomware days after authorities announced that they had … [+3904 chars]"},{"source":{"id":"techcrunch","name":"TechCrunch"},"author":"Eric Schmidt","title":"Winning wireless with American strengths | TechCrunch","description":"America’s infrastructure needs a serious upgrade. That includes the nation’s digital infrastructure — the critical networks underpinning commerce,","url":"https://techcrunch.com/2024/02/23/winning-wireless-with-american-strengths/","urlToImage":"https://techcrunch.com/wp-content/uploads/2017/07/gettyimages-803077724.jpg?resize=1200,801","publishedAt":"2024-02-22T21:27:11Z","content":"Americas infrastructure needs a serious upgrade. That includes the nations digital infrastructure the critical networks underpinning commerce, defense, transport, and public safety.  It sustains inno… [+6993 chars]"},{"source":{"id":"techcrunch","name":"TechCrunch"},"author":"Robert Frawley","title":"Last call — Disrupt 2-for-1 sale ends today | TechCrunch","description":"The clock is ticking on your chance to seize the 2-for-1 ticket deal to TechCrunch Disrupt is returning to San Francisco from October 28–30.","url":"https://techcrunch.com/2024/02/23/last-call-disrupt-2-for-1-sale-ends-today/","urlToImage":"https://techcrunch.com/wp-content/uploads/2024/02/Post_Header_General_1920x1080_7c348d.png?resize=1200,675","publishedAt":"2024-02-21T00:10:35Z","content":"Act quickly, entrepreneurs! The clock is ticking on your chance to seize a scorching deal: TechCrunch Disrupt is returning to San Francisco from October 2830, and the 2-for-1 ticket offer ends tonigh… [+2094 chars]"}]};
 
 const apiKey = "70995abca7ad407ba54495fab30c0526";
+// https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=boolean
+
+
+const botonTrivia = document.getElementById('boton_trivia');
+
+const botonFinTrivia = document.getElementById('end_trivia_button')
+
+
+function validarEntrada(event) {
+    var inputNumero = document.getElementById('numero');
+    var valor = parseInt(inputNumero.value + event.key);
+    var numeroMaximoValido = 50;
+
+    if (!isNaN(valor) && valor > numeroMaximoValido) {
+        alert("Hay un máximo de 50 preguntas")
+        event.preventDefault(); // Evita que se ingrese el número
+        
+        return false;
+    }
+}
+
 
 const getLastMonth = function(){
     const today = new Date();
@@ -31,7 +52,7 @@ const getToday = function(){
     return year + "-" + month + "-" + day;
 }
 
-const peticion = function(url,id){
+const peticionNoticias = function(url,id){
     fetch(url, {
         method : 'GET',
         //mode : 'cors',
@@ -70,7 +91,7 @@ const getNews = function(category, id){
         url = `https://newsapi.org/v2/top-headlines?from=${getLastMonth()}&to=${today}$sortBy=popularity&language=en&category=general&apiKey=${apiKey}`;
     }
     console.log(url);
-    peticion(url, id);
+    peticionNoticias(url, id);
 }
 
 
@@ -90,11 +111,14 @@ const paintNews = function(news, id){
         if (fotoUrl != null)
             htmlCode += `<figure><img src = ${fotoUrl}> </img></figure>`;
         if (descripcion != null)
-            htmlCode += `<p> ${descripcion} </p><figure class = "link_page" > <a href=${urlNoticia}><img src = images/periodico.png alt="Acceder a la página">  </a></figure> `;
+            htmlCode += `<p> ${descripcion} </p><figure class = "boton_periodico" > <a href=${urlNoticia}><img src = images/periodico.png alt="Acceder a la página">  </a></figure> `;
 
         document.getElementById(id).innerHTML = document.getElementById(id).innerHTML + htmlCode;
     }
 }
+
+
+
 
 
 const linkNoticias = document.getElementById("link_noticias");
@@ -150,6 +174,94 @@ linkTecnologia.addEventListener('click', event => {
 
 // ["business", "entertainment", "general", "health", "science", "sports", "technology"];
 
+
+const jugar = function(){
+    
+    const urlParams = new URLSearchParams(window.location.search);
+    const numero = urlParams.get('numero');
+    const categoria= urlParams.get('categoria');
+    const dificultad = urlParams.get('dificultad');
+
+    const url = (categoria!='all') ? `https://opentdb.com/api.php?amount=${numero}&category=${categoria}&difficulty=${dificultad}` : `https://opentdb.com/api.php?amount=${numero}&difficulty=${dificultad}`;
+    fetch(url, {
+        method : 'GET',
+        //mode : 'cors',
+        headers: {'Accept': 'application/json'},
+        //body:  JSON.stringify({ })
+    }).then(respuesta => {
+        if (respuesta.ok){
+            return respuesta.json();
+        }
+        else{
+            throw respuesta.status;
+        }
+    }).then(json => {
+        paintJuego(json);
+        
+        return json;
+    }).catch(error => {
+        //console.error("Error :" + error);
+        return null;
+    });
+    
+}
+// https://opentdb.com/api.php?amount=10&category=10&difficulty=hard&type=boolean
+
+const paintJuego = function(json){
+    botonFinTrivia.style.display = 'block';
+    const areaJuego = document.getElementById('area_preguntas');
+    const preguntas =json.results;
+    let respuestas = [];
+    let allAnswers;
+
+    for (let i=0; i < preguntas.length; i++){
+        areaJuego.innerHTML += `<figure class = "pregunta_trivia" id = "pregunta_trivia_num${i+1}">`
+        areaJuego.innerHTML += `<h3>Question ${i + 1}: ${preguntas[i].question}`
+         
+        allAnswers = preguntas[i].incorrect_answers
+        allAnswers.push(preguntas[i].correct_answer);
+        allAnswers.sort(() => Math.random() - 0.5);
+
+        respuestas.push(allAnswers);
+        for(let j=0; j < allAnswers.length; j++){
+            areaJuego.innerHTML += `
+            <input class= "checkbox_true_false" type="radio" id="respuesta${i+1}${j+1}" name="respuesta${i+1}" value="${allAnswers[j]}"> ${allAnswers[j]}</input>`;
+
+        }
+        areaJuego.innerHTML += `
+            <style>
+            #parrafo_respuesta${i+1} {
+                display: none;
+            }
+            </style>`
+        areaJuego.innerHTML += `<p id="parrafo_respuesta${i+1}"> La respuesta correcta es: ${preguntas[i].correct_answer}</p>`;
+        areaJuego.innerHTML += "</figure>"
+    }
+    botonFinTrivia.addEventListener('click', event =>{
+        event.preventDefault();
+        corregirRespuestas(respuestas);})
+    
+}
+const corregirRespuestas = function(respuestas){
+    
+    let areaRespuestas;
+    let checkboxRespuesta;
+    console.log(respuestas);
+    for (let i=0; i < respuestas.length; i++){
+        areaRespuestas = document.getElementById(`parrafo_respuesta${i+1}`);
+        areaRespuestas.style.display = "block";
+    }
+    
+}
+
+
+/*
+{"response_code":0,"results":[{"type":"boolean","difficulty":"easy","category":"Entertainment: Video Games","question":"In the &quot;S.T.A.L.K.E.R.&quot; series, the Freedom faction wishes to destroy the supernatural area known as  &quot;the Zone&quot;.","correct_answer":"False","incorrect_answers":["True"]},{"type":"boolean","difficulty":"easy","category":"Science: Computers","question":"Linus Torvalds created Linux and Git.","correct_answer":"True","incorrect_answers":["False"]}]}
+*/
+
+
+
+
 const correo = document.getElementById("correo");
 const cuerpo = document.getElementById("cuerpo");
 
@@ -181,3 +293,8 @@ correo.onchange = autoGuardado;
 cuerpo.onchange = autoGuardado;
 
 
+botonTrivia.addEventListener('click', jugar());
+
+
+
+//https://opentdb.com/api.php?amount=2&difficulty=easy&type=boolean
